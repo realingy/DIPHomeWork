d12f8de9c6a217f20987809fa8836c4144cd086e
realingy <754037927@qq.com>
2019-09-11 16:58:30 +0800

    带通/阻滤波器

--- 20190911/1_IdealBand/idealBand.py
+++ 20190911/1_IdealBand/idealBand.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 
 # 带通/带阻滤波器
 
-
 # 理想的带阻/通滤波模板
 def IdealBand(src, w, d0, ftype = 'elimin'):
-    template = np.zeros(src.shape, dtype=np.float32)  # 构建滤波器
     r, c = src.shape
+    template = np.zeros((r, c), np.float32)  # 构建滤波器
     for i in np.arange(r):
         for j in np.arange(c):
             distance = np.sqrt((i - r / 2) ** 2 + (j - c / 2) ** 2)
@@ -26,7 +25,7 @@ src = cv2.imread('test.bmp', 0)
 # 快速傅里叶变换算法得到频率分布
 f = np.fft.fft2(src)
 
-# 默认结果中心点位置是在左上角,
+# 默认结果中心点位置是在左上角
 # 调用fftshift()函数转移到中间位置
 fshift = np.fft.fftshift(f)
 
@@ -39,17 +38,20 @@ plt.axis('off')
 plt.subplot(222), plt.imshow(fsrc, 'gray'), plt.title('Fourier Image')
 plt.axis('off')
 
-fdst = IdealBand(fshift, 10, 0) * fshift
+mask = IdealBand(fshift, 10, 0)
+fshiftdst = mask * fshift
+
+fdst = np.log(np.abs(fshiftdst))
 
 plt.subplot(223), plt.imshow(fdst, 'gray'), plt.title('IdealBand Fourier Image')
 plt.axis('off')
 
-#傅里叶逆变换
-idst = np.fft.ifftshift(fdst)
+# 傅里叶逆变换
+idst = np.fft.ifftshift(fshiftdst)
 dst = np.fft.ifft2(idst)
 dst = np.abs(dst)
 
-cv2.imwrite('dst.bmp', dst)
+# cv2.imwrite('dst.bmp', dst)
 
 plt.subplot(224), plt.imshow(dst, 'gray'), plt.title('Inverse Fourier Fourier')
 plt.axis('off')
