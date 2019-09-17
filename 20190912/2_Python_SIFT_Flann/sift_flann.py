38fb02ba2cba65a391d81e81935ca625ed2ba6ce
realingy <754037927@qq.com>
2019-09-17 11:21:25 +0800

    分离rgb三通道

--- 20190912/2_Python_SIFT_Flann/sift_flann.py
+++ 20190912/2_Python_SIFT_Flann/sift_flann.py
@@ -51,45 +51,78 @@ class macthing(object):
     def panorama_get(self,im1,im2,H):
         h1,w1=im1.shape[:2]
         h2,w2=im2.shape[:2]
-        pts1=np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)#转为3维坐标
+        pts1=np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2) # 转为3维坐标
         pts2=np.float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
-        pts1_=cv2.perspectiveTransform(pts1,H)#H：3*3 矩阵，所以pts1也该为3维坐标
-        pts=np.concatenate((pts1_,pts2),axis=0)#列连接
-          #np.min 是行优先
+        pts1_=cv2.perspectiveTransform(pts1,H) # H：3*3 矩阵，所以pts1也该为3维坐标
+        pts=np.concatenate((pts1_,pts2),axis=0) # 列连接
+        # np.min 是行优先
         [xmin,ymin]=np.int32(pts.min(axis=0).ravel()-0.5)
         [xmax,ymax]=np.int32(pts.max(axis=0).ravel()+0.5)
-        t=[-xmin,-ymin]# 左加右减
-        Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) #相当于一个向右平移
-        result = cv2.warpPerspective(im1, Ht.dot(H), (xmax-xmin, ymax-ymin))#最后一个参数是输出图像的宽、高
+        t=[-xmin,-ymin] # 左加右减
+        Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # 相当于一个向右平移
+        result = cv2.warpPerspective(im1, Ht.dot(H), (xmax-xmin, ymax-ymin)) # 最后一个参数是输出图像的宽、高
         result[t[1]:h2+t[1],t[0]:w2+t[0]] = im2
         return result
 
 
-
-if __name__=="__main__":
-    M=macthing()
-    im1_=cv2.imread(r"left.jpg")
-    im2_=cv2.imread(r"right.jpg")
-    im1=cv2.cvtColor(im1_,cv2.COLOR_BGR2GRAY)
-    im2=cv2.cvtColor(im2_,cv2.COLOR_BGR2GRAY)
-
-    # im1, g1, r1 = cv2.split(im1_)  # 分离函数
-    # im2, g2, r2 = cv2.split(im2_)  # 分离函数
-
-    sift=cv2.xfeatures2d.SIFT_create()
+def work(im1, im2):
+    M = macthing()
+    sift = cv2.xfeatures2d.SIFT_create()
     kp1,des1=sift.detectAndCompute(im1,None)
     kp2,des2=sift.detectAndCompute(im2,None)
     pts1_1,pts2_2,H=M.matchIMG(im1,im2,kp1,kp2,des1,des2)
     im3=M.appendimage(im1,im2)
     pts2_new=pts2_2.copy()
     for i in range(len(pts2_2)):
-        pts2_new[i,0]=pts2_new[i,0]+np.float32(im1.shape[1])
+        pts2_new[i,0]=pts2_new[i,0]+np.float32(b1.shape[1])
     for i in range(len(pts1_1)):
         cv2.line(im3,tuple(pts1_1[i]),tuple(pts2_new[i]),255,2)
 #    cv  cv2\Y\Desktop\45.jpg",result)
     result = M.panorama_get(im1, im2, H)
-    cv2.namedWindow("panorma", cv2.WINDOW_NORMAL)
-    cv2.imshow("panorma",result)
+    return result
+
+
+if __name__=="__main__":
+    # M=macthing()
+    # im1_=cv2.imread(r"left.jpg")
+    # im2_=cv2.imread(r"right.jpg")
+    im1_=cv2.imread(r"left.png")
+    im2_=cv2.imread(r"right.png")
+
+    # im1=cv2.cvtColor(im1_,cv2.COLOR_BGR2GRAY)
+    # im2=cv2.cvtColor(im2_,cv2.COLOR_BGR2GRAY)
+
+    b1, g1, r1 = cv2.split(im1_)  # 分离函数
+    b2, g2, r2 = cv2.split(im2_)  # 分离函数
+
+    b = work(b1, b2)
+    g = work(g1, g2)
+    r = work(r1, r2)
+    cv2.namedWindow("b", cv2.WINDOW_NORMAL)
+    cv2.imshow("b", b)
+    cv2.namedWindow("g", cv2.WINDOW_NORMAL)
+    cv2.imshow("g", g)
+    cv2.namedWindow("r", cv2.WINDOW_NORMAL)
+    cv2.imshow("r", r)
+
+
+#     sift=cv2.xfeatures2d.SIFT_create()
+#     kp1,des1=sift.detectAndCompute(b1,None)
+#     kp2,des2=sift.detectAndCompute(b2,None)
+#     pts1_1,pts2_2,H=M.matchIMG(b1,b2,kp1,kp2,des1,des2)
+#     im3=M.appendimage(b1,b2)
+#     pts2_new=pts2_2.copy()
+#     for i in range(len(pts2_2)):
+#         pts2_new[i,0]=pts2_new[i,0]+np.float32(b1.shape[1])
+#     for i in range(len(pts1_1)):
+#         cv2.line(im3,tuple(pts1_1[i]),tuple(pts2_new[i]),255,2)
+#    cv  cv2\Y\Desktop\45.jpg",result)
+#     result = M.panorama_get(b1, b2, H)
+
+    # result = cv2.merge([b,g,r])
+    # cv2.namedWindow("panorma", cv2.WINDOW_NORMAL)
+    # cv2.imshow("panorma",result)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
